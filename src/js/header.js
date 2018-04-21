eabcf3b15a6a28a7054eca03c75524cb35ef684a
Alona <etna.scorpio@gmail.com>
2018-04-21 21:24:36 +0300

    Finish home page

--- src/js/header.js
+++ src/js/header.js
@@ -1,14 +1,17 @@
 import $ from 'jquery';
 
-var header = $('.js-header');
 var win = $(window);
+var header = $('.js-header');
+var logo = $('.js-header-logo');
 
 function changeHeaderBg() {
   win.scroll(function() {
 	  if( win.scrollTop() > 0 ) {
 	    header.addClass('is-active');
+	    if (logo.length) logo.removeClass('header__logo--move');
 	  } else {
 	    header.removeClass('is-active');
+	    if (logo.length) logo.addClass('header__logo--move');
 	  }
 
   });
