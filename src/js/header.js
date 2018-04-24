489a3fb9c5b8d677c9a19944c38309225b40f9ed
Alona <etna.scorpio@gmail.com>
2018-04-24 12:35:15 +0300

    Add fixes to Home page

--- src/js/header.js
+++ src/js/header.js
@@ -4,17 +4,31 @@ var win = $(window);
 var header = $('.js-header');
 var logo = $('.js-header-logo');
 
+changeHeaderBg();
+transformHeader();
+moveLogo();
+
 function changeHeaderBg() {
   win.scroll(function() {
-	  if( win.scrollTop() > 0 ) {
-	    header.addClass('is-active');
-	    if (logo.length) logo.removeClass('header__logo--move');
-	  } else {
-	    header.removeClass('is-active');
-	    if (logo.length) logo.addClass('header__logo--move');
-	  }
-
+    transformHeader();
+    moveLogo();
   });
 }
 
-changeHeaderBg();
+function transformHeader() {
+  if( win.scrollTop() > 0 ) {
+    header.addClass('is-active');
+  } else {
+    header.removeClass('is-active');
+  }
+}
+
+function moveLogo() {
+  if (logo.length) {
+    if (win.scrollTop() === 0) {
+      logo.addClass('header__logo--move');
+    } else {
+      logo.removeClass('header__logo--move');
+    }
+  }
+}
