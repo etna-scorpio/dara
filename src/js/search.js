946cc33cb6a577693944b4348c3ce10153ed1f3c
Alona <etna.scorpio@gmail.com>
2018-06-06 11:39:55 +0300

    Add fixes for images on product page and product list + fixes for home page

--- src/js/search.js
+++ src/js/search.js
@@ -9,13 +9,13 @@ var closeIcon = $('.js-search-close');
 
 if (search.length) {
   toggleSearchField();
+  clearSearchField();
 
   BODY.click(function(e) {
-    if ( search.val().length <= 0
-				 && !e.target.closest(searchBox)
-				 && $(searchBox).hasClass('is-active')) {
-      console.log('body click');
+    if (!e.target.closest(searchBox) && $(searchBox).hasClass('is-active')) {
       hideSearchField();
+      hideAutocomplete();
+      closeIcon.hide();
     }
   });
 }
@@ -30,20 +30,13 @@ search.on('keyup', function() {
   }
 });
 
-// search.on('blur', function() {
-//   if($(this).val().length < 1) {
-//     hideSearchField();
-//     hideAutocomplete();
-//   }
-// });
-
 function toggleSearchField() {
   searchIcon.click(function() {
     $(searchBox).toggleClass('is-active');
     if ($(searchBox).hasClass('is-active')) {
       search.focus();
+      if(search.val().length > 0) closeIcon.show();
     } else {
-      search.val('');
       closeIcon.hide();
       hideAutocomplete();
     }
@@ -62,4 +55,11 @@ function hideSearchField() {
   $(searchBox).removeClass('is-active');
 }
 
-
+function clearSearchField() {
+  closeIcon.click(function() {
+    search.val('');
+    search.focus();
+    closeIcon.hide();
+    hideAutocomplete();
+  });
+}
